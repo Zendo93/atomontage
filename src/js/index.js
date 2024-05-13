b48479bfbc897656f9b7d9afc0393268c5c94b9c
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2024-05-13 12:21:54 +0200

    post request

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,18 @@ $(document).on('click','#menuButton',function(){
 
     $('#menu').hide();
 });
+
+
+$(document).on('click','#sendUserBtn',function(){
+    $.post( "/api/user.php", { 
+            'first_name': $('#grid-first-name').val(), 
+            'last_name': $('#grid-last-name').val(),
+            password: $('#grid-password').val(),
+            city: $('#grid-city').val(),
+            state: $('#grid-state').val(),
+            zip: $('#grid-zip').val() 
+        })
+        .done(function( data ) {
+            alert( "Data sent successfully " + data );
+        });
+});
