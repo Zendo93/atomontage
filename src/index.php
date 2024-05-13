4e88b7c5372cc5d5a313a32ca71d7f9024a1c641
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2024-05-13 09:05:21 +0200

    table

--- src/index.php
+++ src/index.php
@@ -10,104 +10,43 @@
     <?php
         include('header.php')
     ?>
-    <form class="w-full max-w-lg m-auto p-6">
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                First Name
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
-            <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-            </div>
-            <div class="w-full md:w-1/2 px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                Last Name
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
-            </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                Password
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************">
-            <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
-            </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-2">
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                City
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque">
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                State
-            </label>
-            <div class="relative">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                <option>New Mexico</option>
-                <option>Missouri</option>
-                <option>Texas</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                </div>
-            </div>
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                Zip
-            </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210">
-            </div>
-        </div>
-    </form>
     <div class="p-6">
-        <table class="table-auto m-auto">
+        <table class="table-auto m-auto text-[0.65rem] sm:text-base">
             <thead>
                 <tr>
-                <th class="px-4 py-2">Title</th>
-                <th class="px-4 py-2">Author</th>
-                <th class="px-4 py-2">Views</th>
+                <th class="px-4 py-2">First Name</th>
+                <th class="px-4 py-2">Last Name</th>
+                <th class="px-4 py-2">City</th>
+                <th class="px-4 py-2">State</th>
+                <th class="px-4 py-2">ZIP</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <td class="border px-4 py-2">Intro to CSS</td>
-                <td class="border px-4 py-2">Adam</td>
-                <td class="border px-4 py-2">858</td>
-                </tr>
-                <tr class="bg-gray-100">
-                <td class="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-                <td class="border px-4 py-2">Adam</td>
-                <td class="border px-4 py-2">112</td>
-                </tr>
-                <tr>
-                <td class="border px-4 py-2">Intro to JavaScript</td>
-                <td class="border px-4 py-2">Chris</td>
-                <td class="border px-4 py-2">1,280</td>
-                </tr>
+            <?php
+                require '../vendor/autoload.php';
+                $database = new Dibi\Connection([
+                    'driver'   => 'mysqli',
+                    'host'     => 'localhost',
+                    'port'     =>  4000,     
+                    'username' => 'root',
+                    'password' => 'admin',
+                    'database' => 'atomontage',
+                ]);
+                
+                $result = $database->query('SELECT * FROM user');
+                $all = $result->fetchAll();
+
+                foreach ($all as $key => $row): ?>
+                    <tr class="bg-gray-100">
+                        <td class="border px-4 py-2"><?= $row['first_name']; ?></td>
+                        <td class="border px-4 py-2"><?= $row['last_name']; ?></td>
+                        <td class="border px-4 py-2"><?= $row['city']; ?></td>
+                        <td class="border px-4 py-2"><?= $row['state']; ?></td>
+                        <td class="border px-4 py-2"><?= $row['zip']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-  <?php
-    require '../vendor/autoload.php';
-    echo "Hello World!";
-    $database = new Dibi\Connection([
-        'driver'   => 'mysqli',
-        'host'     => 'localhost',
-        'port'     =>  4000,     
-        'username' => 'root',
-        'password' => 'admin',
-        'database' => 'atomontage',
-    ]);
-    
-    $result = $database->query('SELECT * FROM user');
-    $all = $result->fetchAll();
-    echo  $all[0]['first_name'];
-    ?>
 </body>
 </html>
